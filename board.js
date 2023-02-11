3f7152d646a9c03f1e0ecb0f6ca5339638e57cd8
Alexo-157 <101476452+Alexo-157@users.noreply.github.com>
2023-02-11 18:04:25 +0000

    centered the board and fixed the placement function

--- board.js
+++ board.js
@@ -1,9 +1,17 @@
 const board = document.getElementById("chess-board");
+const parentRect = board.getBoundingClientRect() 
+let nodeArray = [] 
+let leftArray = []
+let topArray = []
+
+let distanceX = parentRect.x
+let distanceY = parentRect.y 
 
 
 for (let i = 0; i < 8; i++) {
   for (let j = 0; j < 8; j++) {
     var square = document.createElement("div");
+    nodeArray.push(square)
     square.className = "chess-block";
     square.id = `${i}${j}`;
     if ((i + j) % 2 == 0) {
@@ -13,13 +21,33 @@ for (let i = 0; i < 8; i++) {
   }
 }
 
-const placement = function (pos){
-  return(Math.round(pos/60)*60)
+for(let k = 0; k < nodeArray.length; k++){
+  leftArray.push(nodeArray[k].offsetLeft)
+  topArray.push(nodeArray[k].offsetTop)
+}
 
-};
+const placement = function(pos){
+  let closest = null 
+  let minDist = 10000
+  for(let p = 0; p < nodeArray.length; p++){
+    const distX = Math.abs(pos.offsetLeft - leftArray[p]);
+    const distY = Math.abs(pos.offsetTop - topArray[p]);
+    const dist = Math.sqrt(distX * distX + distY * distY);
+    if (dist < minDist) {
+      minDist = dist;
+      closest = p;
+    }
+    
+  }
+  return(closest)
+
+}
+
+
 
 const valid = function(piece){
-  if(((parseInt(piece.style.top) < 449 )&&( parseInt(piece.style.left) < 449))&&((parseInt(piece.style.top) >= 0 )&&( parseInt(piece.style.left) >= 0))){
+  if((parseInt(piece.offsetTop) < (distanceY + 449)&&(parseInt(piece.offsetLeft) < (449 + distanceX)))
+  &&((parseInt(piece.offsetTop) >= (0 + distanceY) )&&( parseInt(piece.offsetLeft) >= (0+distanceX)))){
     return true
     //you can later use this to check if the move is valid by the rules
   }else{
@@ -33,23 +61,20 @@ dragElement(document.querySelector('.piece'))
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  /* otherwise, move the DIV from anywhere inside the DIV:*/
     elmnt.onmousedown = dragMouseDown;
     let top = elmnt.style.top 
     let lefty = elmnt.style.left
-    console.log(elmnt.style.top, elmnt.style.left)
+
   
 
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();
-    // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
     ogx = e.clientX
     ogy = e.clientY
     document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
   }
 
@@ -65,22 +90,19 @@ function dragElement(elmnt) {
     
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    //elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
   }
 
   function closeDragElement(e) {
     /* stop moving when mouse button is released:*/
     if(valid(elmnt)){
-      elmnt.style.top = placement(elmnt.offsetTop - pos2)+10 + "px";
-      elmnt.style.left = placement(elmnt.offsetLeft - pos1)+7 + "px";
+      let next = placement(elmnt)
+      elmnt.style.left = leftArray[next] + 'px'
+      elmnt.style.top = topArray[next] + 'px'
+    
     }else{
       elmnt.style.top = top 
       elmnt.style.left = lefty
     }
-    // elmnt.style.top = valid(elmnt) ? placement(parseInt(elmnt.style.top)) : top
-    // elmnt.style.left = valid(elmnt) ? placement(parseInt(elmnt.style.left)) : lefty  
-    // elmnt.style.top = placement(elmnt.offsetTop - pos2)+10 + "px";
-    // elmnt.style.left = placement(elmnt.offsetLeft - pos1)+7 + "px";
     
     document.onmouseup = null;
     document.onmousemove = null;
@@ -91,9 +113,6 @@ function dragElement(elmnt) {
 }
 
 
-//just give everything rules
-//add the you can't move until it is your turn
-//put all the logic in the placement function 
 
 
 
